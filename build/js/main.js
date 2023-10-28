945451e89131e6c6de6e1240285c8c16f3900ad0
Chaitanya Vardhan <chaitanyavardhan1@gmail.com>
2023-10-28 16:41:59 +0530

    chore:repo-setup

---
+++ build/js/main.js
@@ -0,0 +1,7 @@
+"use strict";
+var userName = "Chaitanya";
+console.log(userName);
+let a = 12;
+let b = '6';
+let c = 2;
+console.log(a, b, c);
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+var userName = "Chaitanya";
+console.log(userName);
+
+let a = 12;
+let b = '6'
+let c = 2
+console.log(a, b, c)
