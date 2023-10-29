4ad59ae6c8d4172c6f957073ed2872d87f715759
Chaitanya Vardhan <chaitanyavardhan1@gmail.com>
2023-10-29 23:59:45 +0530

    Classes in typescript

--- build/js/main.js
+++ build/js/main.js
@@ -2,6 +2,17 @@
 var userName = "Chaitanya";
 console.log(userName);
 let a = 12;
-let b = '6';
+let b = 6;
 let c = 2;
 console.log(a, b, c);
+console.log(a + c);
+console.log(a / b);
+console.log(b * c);
+class A {
+    constructor(name, age, gpa) {
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+    }
+}
+console.log(new A('Chaitanya Vardhan', 43, 6.74));
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,32 @@
 var userName = "Chaitanya";
 console.log(userName);
 
-let a = 12;
-let b = '6'
+let a: number = 12;
+let b: number = 6
 let c = 2
 console.log(a, b, c)
+console.log(a + c)
+console.log(a / b)
+console.log(b * c)
+// lesson2
+//lesson7
+// defining a class
+interface Student {
+    name: string,
+    age: number,
+    gpa: number,
+}
+
+class A implements Student {
+    name: string;
+    age: number;
+    gpa: number;
+
+    constructor(name: string, age: number, gpa: number){
+        this.name = name;
+        this.age = age;
+        this.gpa = gpa;
+    }
+}
+
+console.log(new A('Chaitanya Vardhan', 43, 6.74))
